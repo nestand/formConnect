507b5b69a54c183c9bfd86eb27c1695c334e3a76
nestand <andrey.nesterov@laposte.net>
2021-05-06 16:28:13 +0200

    first commit

---
+++ login.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+        
+    </head>
+    <body>
+    <body>
+<video playsinline autoplay muted loop class="bgvideo" id="bgvideo">
+  <source src="testbg.mp4" type="video/mp4">
+</video>
+        <div id="container">
+            <!-- notre form et zone de connexion -->
+            
+            <form action="verification.php" method="POST">
+                <h1>Connexion</h1>
+                
+                <label><b>Nom d'utilisateur</b></label>
+                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+
+                <label><b>Mot de passe</b></label>
+                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+
+                <input type="submit" id='submit' value='LOGIN' >
+                <input type="reset"  id='reset' value= 'RESET'>
+                <?php
+                if(isset($_GET['erreur'])){
+                    $err = $_GET['erreur'];
+                    if($err==1 || $err==2)
+                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                }
+                ?>
+            </form>
+        </div>
+    </body>
+</html>
